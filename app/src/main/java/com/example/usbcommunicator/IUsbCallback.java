4e9402c0f6cee03dba529798b6fa2e1cce58b731
Kekega <saikin.ao@phystech.edu>
2024-01-10 13:45:39 +0300

    kotlin added

--- app/src/main/java/com/example/usbcommunicator/IUsbCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.usbcommunicator;
-
-public interface IUsbCallback {
-    void onConnectionEstablished() ;
-
-    void onDeviceDisconnected() ;
-
-    void onDataReceived(byte[] data, int num) ;
-}
